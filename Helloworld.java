9aacccf6e1ee6562dd8fbc238afe7685d9e4abe8
sreeja-8 <konankisreeja@gmail.com>
2021-09-13 11:15:05 +0530

    created helloworld

---
+++ Helloworld.java
@@ -0,0 +1,8 @@
+public class Helloworld{
+    public static void main(String[] s){
+        System.out.println("Helloworld");
+        for(int i=1;i<=10;i++){
+            System.out.println("Value of I is : "+i);
+        }
+    }
+}
